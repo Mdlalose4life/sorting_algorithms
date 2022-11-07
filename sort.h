ca001a442a6cd653e1ee5d48551e03de02e2d472
Mdlalose4life <blessing1059@gmail.com>
2022-11-07 18:35:00 +0200

    Test commit -m

---
+++ sort.h
@@ -0,0 +1,49 @@
+#ifndef_SORT_H
+#define_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
+}
+
+#endif
