1c1e42b003081fb7176b33b07db09062ce42348c
root <root@DESKTOP-SOMR5L8.localdomain>
2022-11-08 12:28:53 +0200

    project push

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * swap - swaps two elements in an array
+ * @xp: first value to be swapped
+ * @yp: second value to be swapped
+ * Return: Always 0.
+ */
+void swap(int *xp, int *yp)
+{
+  int temp;
+  temp = *xp;
+  *xp = *yp;
+  *yp = temp;
+}
+
+/**
+ * bubble_sort - This function performs bubble sort in an array
+ * @array: A pointer to an array that need to be sorted.
+ * @size: The size of an array to be sorted.
+ * Return: Always 0.
+ */
+void bubble_sort(int *array, size_t size)
+{
+  size_t j, pos;
+  if (array == NULL || size < 2)
+    return;
+  
+  while(size > 0)
+    {
+    pos = 0;
+    for(j = 0; j < size - 1; j++)
+      if(array[j] > array[j + 1])
+      {
+        swap(&array[j], &array[j + 1]);
+        pos = j + 1;
+        print_array(array, size);
+      }
+    }
+    size = pos;
+}
--- sort.h
+++ sort.h
@@ -1,49 +1,26 @@
-#ifndef_SORT_H
-#define_SORT_H
+#ifndef SORT_H
+#define SORT_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_array - Prints an array of integers
+ * struct listint_s - Doubly linked list node
  *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
-void print_array(const int *array, size_t size)
+typedef struct listint_s
 {
-    size_t i;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif
